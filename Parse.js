bef95a8794ce213d27236761a0e08aecc3211e6d
Chad <chadclark1@gmail.com>
2014-08-14 17:17:48 -0700

    Second Form, surrey
    Second email form now works.
    Qualaroo-type survey.
    JS beautifier

--- Parse.js
+++ Parse.js
@@ -4,95 +4,95 @@ $(document).ready(function() {
 
 
 
-	$(".input-group-btn").click(function() {
-		console.log("Notify Me");
+    $(".input-group-btn").click(function() {
+        console.log("Notify Me");
 
-       var emailAdd = $(".form-control").val();
+        var emailAdd = $(".form-control").val();
 
-            if (emailAdd.length <5) {
-               alert("Please enter a valid email address.");
+        if (emailAdd.length < 5) {
+            alert("Please enter a valid email address.");
 
 
         } else {
 
-		var Address = $(".form-control").val();
+            var Address = $(".form-control").val();
 
-		var Email = Parse.Object.extend("Email");
-		var email = new Email();
- 
-		email.set("Address", Address);
+            var Email = Parse.Object.extend("Email");
+            var email = new Email();
 
-		console.log(Address);
+            email.set("Address", Address);
 
-		email.save(null, {
-        	success: function(email) {
-          	// Execute any logic that should take place after the object is saved.
-          	console.log('New object created with objectId: ' + email.id);
-        	// Invoke our cloud function, using the phone number in the text field
+            console.log(Address);
 
-              Parse.Cloud.run('SubscribeUserToMailingList', {
-                    listid      : "9748c09977",
-                    email       : $(".form-control").val()
-                })
-                .then(function(success){
-                    console.log("Successfully subscribed");
-                    // ...
-                },
-                function(error){
-                    console.log("Unable to subscribe");
-                    // ...
-                });
+            email.save(null, {
+                success: function(email) {
+                    // Execute any logic that should take place after the object is saved.
+                    console.log('New object created with objectId: ' + email.id);
+                    // Invoke our cloud function, using the phone number in the text field
 
-
-
-        	Parse.Cloud.run('MandrillSubscribe', {
-            	Address: $(".form-control").val()
-        		}, {
-            		// Success handler
-            		success: function(message) {
-                		alert("Thank you! Please check your inbox for news about People Clothing.");
-            		},
-            		// Error handler
-            		error: function(message) {
-               	 		alert('Error: ' + message);
-            		}
-        		});
-        	}
+                    Parse.Cloud.run('SubscribeUserToMailingList', {
+                            listid: "9748c09977",
+                            email: $(".form-control").val()
+                        })
+                        .then(function(success) {
+                                console.log("Successfully subscribed");
+                                // ...
+                            },
+                            function(error) {
+                                console.log("Unable to subscribe");
+                                // ...
+                            });
 
 
-    	});
-	}	
+
+                    Parse.Cloud.run('MandrillSubscribe', {
+                        Address: $(".form-control").val()
+                    }, {
+                        // Success handler
+                        success: function(message) {
+                            alert("Thank you! Please check your inbox for news about People Clothing.");
+                        },
+                        // Error handler
+                        error: function(message) {
+                            alert('Error: ' + message);
+                        }
+                    });
+                }
+
+
+            });
+        }
     });
 
-	$(".btn-wide").click(function() {
-		console.log("Buy");
-	});
+    $(".btn-wide").click(function() {
+        console.log("Buy");
+    });
 
 
     var handler = StripeCheckout.configure({
         key: 'pk_test_4TzBTXvA3s0wWq0WfUyJgGQv',
         image: '../../startup/common-files/img/PLogoStripe.png',
         token: function(token) {
-        // Use the token to create the charge with a server-side script.
-        // You can access the token ID with `token.id`
-        
-        Parse.Cloud.run('preOrder', {
-            token: token.id
-        }, {
-            // Success handler
+            // Use the token to create the charge with a server-side script.
+            // You can access the token ID with `token.id`
+
+            Parse.Cloud.run('preOrder', {
+                token: token.id
+            }, {
+                // Success handler
                 success: function(message) {
                     alert('Thank you for your purchase. Please check your email for more details.');
                     console.log(token.email);
 
-                            Parse.Cloud.run('SubscribeUserToMailingList', {
-                                listid      : "9748c09977",
-                                email       : token.email
-                            })
-                            .then(function(success){
+                    Parse.Cloud.run('SubscribeUserToMailingList', {
+                            listid: "9748c09977",
+                            email: token.email
+                        })
+                        .then(function(success) {
                                 console.log("Successfully subscribed");
                                 // ...
                             },
-                            function(error){
+                            function(error) {
                                 console.log("Unable to subscribe");
                                 // ...
                             });
@@ -104,7 +104,7 @@ $(document).ready(function() {
 
                     var Email = Parse.Object.extend("Email");
                     var email = new Email();
- 
+
                     email.set("Address", Address);
 
                     console.log(Address + '5');
@@ -116,9 +116,9 @@ $(document).ready(function() {
                             console.log(token.email);
 
 
-                        // Invoke our cloud function, using the phone number in the text field
-                        Parse.Cloud.run('MandrillPreOrder', {
-                            Address: token.email
+                            // Invoke our cloud function, using the phone number in the text field
+                            Parse.Cloud.run('MandrillPreOrder', {
+                                Address: token.email
                             }, {
                                 // Success handler
                                 success: function(message) {
@@ -126,7 +126,7 @@ $(document).ready(function() {
                                 },
                                 // Error handler
                                 error: function(message) {
-                            console.log('Error: ' + message);
+                                    console.log('Error: ' + message);
                                 }
                             });
                         }
@@ -137,15 +137,15 @@ $(document).ready(function() {
                 error: function(message) {
                     alert('Error: 2' + message);
                 }
-             })
+            })
 
 
-        Parse.Cloud.run('createCustomer',{
-            token: token.id,
-            email: token.email,
-            name: token.card.name,
-        }, {
-            // Success handler
+            Parse.Cloud.run('createCustomer', {
+                token: token.id,
+                email: token.email,
+                name: token.card.name,
+            }, {
+                // Success handler
                 success: function(message) {
                     console.log('Success: 3' + message);
 
@@ -154,93 +154,94 @@ $(document).ready(function() {
                 error: function(message) {
                     console.log('Error: 4' + message);
                 }
-        })
+            })
         }
     });
 
     document.getElementById('btn-wide').addEventListener('click', function(e) {
         // Open Checkout with further options
         handler.open({
-        name: 'People Clothing',
-        description: 'Pre-Order One Month of T-Shirts',
-        amount: 3500,
-        shippingAddress: true,
-        billingAddress:true,
-        panelLabel: 'Pre-Order',
+            name: 'People Clothing',
+            description: 'Pre-Order One Month of T-Shirts',
+            amount: 3500,
+            shippingAddress: true,
+            billingAddress: true,
+            coupon: true,
+            panelLabel: 'Pre-Order',
         });
         e.preventDefault();
-  	});
+    });
 
 
 
 
-
-    $(".btn-large").click(function() {
+    $(".subscribe2").click(function() {
         console.log("Notify Me");
 
-       var emailAdd = $(".subinput").val();
+        var emailAdd = $(".subinput").val();
 
-            if (emailAdd.length <5) {
-               alert("Please enter a valid email address.");
+        if (emailAdd.length < 5) {
+            alert("Please enter a valid email address.");
 
 
         } else {
 
-        var Address = $(".subinput").val();
+            var Address = $(".subinput").val();
 
-        var Email = Parse.Object.extend("Email");
-        var email = new Email();
- 
-        email.set("Address", Address);
+            var Email = Parse.Object.extend("Email");
+            var email = new Email();
 
-        console.log(Address);
+            email.set("Address", Address);
 
-        email.save(null, {
-            success: function(email) {
-            // Execute any logic that should take place after the object is saved.
-            console.log('New object created with objectId: ' + email.id);
-            // Invoke our cloud function, using the phone number in the text field
+            console.log(Address);
 
-              Parse.Cloud.run('SubscribeUserToMailingList', {
-                    listid      : "9748c09977",
-                    email       : $(".subinput").val()
-                })
-                .then(function(success){
-                    console.log("Successfully subscribed");
-                    // ...
+            email.save(null, {
+                success: function(email) {
+                    // Execute any logic that should take place after the object is saved.
+                    console.log('New object created with objectId: ' + email.id);
+                    // Invoke our cloud function, using the phone number in the text field
+
+                    Parse.Cloud.run('SubscribeUserToMailingList', {
+                            listid: "9748c09977",
+                            email: $(".subinput").val()
+                        })
+                        .then(function(success) {
+                                console.log("Successfully subscribed");
+                                // ...
+                            },
+                            function(error) {
+                                console.log("Unable to subscribe");
+                                // ...
+                            });
+
+
+
+                    Parse.Cloud.run('MandrillSubscribe', {
+                        Address: $(".subinput").val()
+                    }, {
+                        // Success handler
+                        success: function(message) {
+                            alert("Thank you! Please check your inbox for news about People Clothing.");
+                        },
+                        // Error handler
+                        error: function(message) {
+                            alert('Error: ' + message);
+                        }
+                    });
                 },
-                function(error){
-                    console.log("Unable to subscribe");
-                    // ...
-                });
-
-
-
-            Parse.Cloud.run('MandrillSubscribe', {
-                Address: $(".subinput").val()
-                }, {
-                    // Success handler
-                    success: function(message) {
-                        alert("Thank you! Please check your inbox for news about People Clothing.");
-                    },
-                    // Error handler
-                    error: function(message) {
-                        alert('Error: ' + message);
-                    }
-                });
-            },
 
                 error: function(email, error) {
-                console.log('Could not accept email address: ' + error.message);
-                alert("Error")
-            },
+                    console.log('Could not accept email address: ' + error.message);
+                    alert("Error")
+                },
 
-        });
-    }
+            });
+        }
 
 
 
-    })	  
+    });
+
 });
 
 
